36c45942c6e3f33475c0f762ecadf0c247c9b970
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-04-26 16:00:54 -0400

    fix: correct typo in TimeStampModel inheritance

--- investments/models.py
+++ investments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class TimeStampModel(models.model):
+class TimeStampModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # TODO: add deleted_at if neccesary later

ac84feb24f4c0adf5968a06b0974013afdedce2c
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-04-27 18:07:33 -0400

    feat: add calculate_initial_quantities command

---
+++ investments/management/commands/calculate_initial_quantities.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from investments.services import calculate_initial_quantities
+
+
+class Command(BaseCommand):
+    help = 'Calculate initial quantities for portfolios'
+
+    def handle(self, *args, **kwargs):
+        try:
+            self.stdout.write('Running initial quantity calculation...')
+            calculate_initial_quantities()
+            self.stdout.write(self.style.SUCCESS('Successfully calculated initial quantities'))
+        
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error calculating initial quantities: {str(e)}'))

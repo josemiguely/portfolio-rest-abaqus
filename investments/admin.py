abf7d3ceaa39dd23b57c4341221aef1e26eb9266
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-04-27 17:58:40 -0400

    feat: register quantity model in admin panel

--- investments/admin.py
+++ investments/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Asset, Price, Weight, Portfolio
+from .models import Asset, Price, Weight, Portfolio, Quantity
 
 admin.site.register(Portfolio)
 admin.site.register(Asset)
 admin.site.register(Price)
 admin.site.register(Weight)
-
+admin.site.register(Quantity)

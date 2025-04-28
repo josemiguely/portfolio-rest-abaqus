747e4519679ff6eba73dcb097cc05b5f98e865dd
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-04-27 23:24:23 -0400

    refactor: add versioning to endpoint

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('investments.urls')),
+    path('api/v1/', include('investments.urls')),
 ]
